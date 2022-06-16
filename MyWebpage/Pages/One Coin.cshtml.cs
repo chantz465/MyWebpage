e0696474e0b0a9016e1ba26b543444d03aaf1753
Chance Gardner <Chantz465@gmail.com>
2022-06-16 08:46:45 -0600

    frist add

---
+++ MyWebpage/Pages/One Coin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebpage.Pages
+{
+    public class One_CoinModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyWebpage/Pages/Tracking Crypto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebpage.Pages
+{
+    public class Tracking_CryptoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
